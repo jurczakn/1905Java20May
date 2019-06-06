60682a30282b1e42d417b9403372d6d54ee7fc32
jurczakn <nickolas.jurczak@gmail.com>
2019-06-06 14:31:16 -0400

    finished logging

--- Week1/MavenExample/src/main/java/com/revature/example/driver/LoggingDriver.java
+++ Week1/MavenExample/src/main/java/com/revature/example/driver/LoggingDriver.java
@@ -6,6 +6,11 @@ public class LoggingDriver {
 
 	public static void main(String[] args) {
 		LoggingUtil.trace("This is a trace log");
+		LoggingUtil.debug("This is a debug log");
+		LoggingUtil.info("This is an info log");
+		LoggingUtil.warn("This is a warn log");
+		LoggingUtil.error("This is an error log");
+		LoggingUtil.fatal("This is a fatal log");
 	}
 
 }
--- Week1/MavenExample/src/main/java/com/revature/example/util/LoggingUtil.java
+++ Week1/MavenExample/src/main/java/com/revature/example/util/LoggingUtil.java
@@ -9,5 +9,25 @@ public class LoggingUtil {
 	public static void trace(String msg) {
 		log.trace(msg);
 	}
+	
+	public static void debug(String msg) {
+		log.debug(msg);
+	}
+	
+	public static void info(String msg) {
+		log.info(msg);
+	}
+	
+	public static void warn(String msg) {
+		log.warn(msg);
+	}
+	
+	public static void error(String msg) {
+		log.error(msg);
+	}
+	
+	public static void fatal(String msg) {
+		log.fatal(msg);
+	}
 
 }
